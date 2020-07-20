3c5cbb1d2f4c928bd0f5d28c94dc5bbf756552c5
vijaykrisin <68561185+vijaykrisin@users.noreply.github.com>
2020-07-20 23:57:48 +0530

    Create Day1 Assignment.js

---
+++ Day1 Assignment.js
@@ -0,0 +1,13 @@
+What is Python? What are the ways to Install Python in your PC?
+Ans: Python is a general purpose programming language. It is Object Oriented, functional programming. The language is very simple and anybody could learn.
+The software was the brain child of Guido van Rossum who created in 1990, however it had become popular only by 2006 when Google started to support it.The other reason could
+be python is memory or RAM intensive and hardware was expensive before 2000, and when the hardware became cheaper more companies started to adopt it. 
+
+Ways to Install Python in PC: 
+>>Go to Anaconda.com, install the latest 32 or 64 bit installer to your system. Once installed, you can open Anaconda Navigator which is a desktop GUI that comes with Anaconda 
+Individual Edition. It makes it easy to launch applications and manage packages and environments without using command-line commands.
+>> Python.org\downloads to install the latest python version to your system. The basic difference between Idle notebook of Python and Jupyter notebook is that while in IDLE you need to import libraries as an when needed, whereas in Jupyter it already has all the libraries included, which automatically helps to write your code faster and lesser lines of code.
+
+What are Code Environment and Types?
+Code Environment is where one writes the code, compile and generate the output .
+There are 3 types Text Editor, Full IDE's, Notebook Environments.
